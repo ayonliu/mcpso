9e900b761826bb90e2f0e0124da67468112b076b
Yong Liu <ayon.liu@gmail.com>
2025-07-11 14:17:45 +0800

    add assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = (phase) => {
   // After running next build, Next.js will produce an out folder which contains the HTML/CSS/JS assets for your application.
   // output: "export",
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+  const nextConfig = {
+    assetPrefix: isDev ? "" : process.env.NEXT_PUBLIC_ASSET_PREFIX,
+  };
+  return nextConfig;
 };
 
 if (process.env.NODE_ENV === "development") {
